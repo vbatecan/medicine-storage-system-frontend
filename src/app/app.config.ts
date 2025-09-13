579007773eab9580bb8ba70d73388317422a0799
Vince Angelo Batecan <llTheBlankll@gmail.com>
2025-09-13 22:23:07 +0800

    add Tailwind CSS and PrimeNG integration; update routing and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: ""
+        }
+      }
+    })
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pharmacist/home',
+    loadComponent: () => import('./routes/pharmacist/home/home').then(m => m.Home)
+  },
+  {
+    path: 'kiosk/interface',
+    loadComponent: () => import('./routes/kiosk/kiosk-interface/kiosk-interface').then(m => m.KioskInterface)
+  }
+];
