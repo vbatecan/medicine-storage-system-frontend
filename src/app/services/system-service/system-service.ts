68d17ccdf7ae3cb65c5332a223033f41421db3b8
Vince Angelo Batecan <llTheBlankll@gmail.com>
2025-09-14 13:40:30 +0800

    add system service with health check functionality

---
+++ src/app/services/system-service/system-service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SystemService {
+
+  private readonly API_URL = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+
+  isSystemHealthy() {
+    return this.http.get(`${this.API_URL}`);
+  }
+}
