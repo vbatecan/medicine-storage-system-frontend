f885bad0f45a2993b5ca525feed9d9f6d0798cde
Vince Angelo Batecan <llTheBlankll@gmail.com>
2025-09-14 13:40:56 +0800

    fix: correct component name in pharmacist route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'pharmacist/home',
-    loadComponent: () => import('./routes/pharmacist/home/pharmacist-home.component').then(m => m.PharmacistHome)
+    loadComponent: () => import('./routes/pharmacist/home/pharmacist-home.component').then(m => m.PharmacistHomeComponent)
   },
   {
     path: 'kiosk/interface',
     loadComponent: () => import('./routes/kiosk/kiosk-interface/kiosk-interface').then(m => m.KioskInterface)
+  },
+  {
+    path: 'it-admin/home',
+    loadComponent: () => import('./routes/it-admin/it-admin-home/it-admin-home').then(m => m.ItAdminHome)
   }
 ];
