20e42f1ccb0771377cf74c9ae8b29883a45d2d03
Vince Angelo Batecan <llTheBlankll@gmail.com>
2025-09-14 11:40:10 +0800

    refactor home component to pharmacist-home; update file names and imports

--- src/app/routes/pharmacist/home/pharmacist-home.component.spec.ts
+++ src/app/routes/pharmacist/home/pharmacist-home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Home } from './home';
+import { PharmacistHome } from './pharmacist-home.component';
 
-describe('Home', () => {
-  let component: Home;
-  let fixture: ComponentFixture<Home>;
+describe('PharmacistHome', () => {
+  let component: PharmacistHome;
+  let fixture: ComponentFixture<PharmacistHome>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
+      imports: [PharmacistHome]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Home);
+    fixture = TestBed.createComponent(PharmacistHome);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/routes/pharmacist/home/pharmacist-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pharmacist-home',
+  imports: [],
+  templateUrl: './pharmacist-home.component.html',
+  styleUrl: './pharmacist-home.component.css'
+})
+export class PharmacistHome {
+
+}
